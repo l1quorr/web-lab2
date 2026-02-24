eff29836f18e4d102cf9654f70acec0779d66bef
Andrii Leonov <leonov.andriy2005@gmail.com>
2026-02-24 10:41:35 +0200

    Synchronized React components with final HTML

--- lab_2_react/src/components/Header.jsx
+++ lab_2_react/src/components/Header.jsx
@@ -2,13 +2,19 @@ function Header() {
     return (
         <header>
             <h1>Андрій Леонов</h1>
-            <p>Студент спеціальності "Кібербезпека" | Львівська Політехніка</p>
-            <ul>
-                <li>Email: <a href="mailto:leonov.andriy2005@gmail.com">leonov.andriy2005@gmail.com</a></li>
-                <li>Університетська пошта: <a href="mailto:andrii.leonov.kb.2023@lpnu.ua">andrii.leonov.kb.2023@lpnu.ua</a></li>
-                <li>GitHub: <a href="https://github.com/l1quorr" target="_blank">github.com/l1quorr</a></li>
-            </ul>
+            <p>Студент спеціальності "Кібербезпека" (Львівська Політехніка)</p>
+            <nav>
+                <ul>
+                    <li>Email: <a href="mailto:leonov.andriy2005@gmail.com">leonov.andriy2005@gmail.com</a></li>
+                    <li>Uni Email: <a href="mailto:andrii.leonov.kb.2023@lpnu.ua">andrii.leonov.kb.2023@lpnu.ua</a></li>
+                    <li>GitHub: <a href="https://github.com/l1quorr" target="_blank">github.com/l1quorr</a></li>
+                </ul>
+            </nav>
             <hr />
+            <section>
+                <h2>Профіль</h2>
+                <p>Студент кафедри захисту інформації (вступ 2023 року). Цікавлюся мережевою безпекою, OSINT та низькорівневим програмуванням. Маю навички адміністрування Windows/Linux та розробки захищеного ПЗ.</p>
+            </section>
         </header>
     );
 }
